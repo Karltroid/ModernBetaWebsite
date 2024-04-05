d717512080a3928554721c64e72aec9afcb8584c
Karl <KarltroidContact@Gmail.com>
2024-04-04 21:44:53 -0400

    Mobile nav side bar functionality!

---
+++ scripts/mobilenav.js
@@ -0,0 +1,29 @@
+var mobileNavSideBar;
+
+window.onload = function()
+{
+	mobileNavSideBar = document.getElementById("mobile-nav-dropdown");
+	closenav();
+};
+
+
+function togglenav()
+{
+	console.log(mobileNavSideBar);
+	mobileNavSideBar.style.display == "none" ? opennav() : closenav();
+}
+
+function opennav()
+{
+	mobileNavSideBar.style.display = "block";
+}
+
+function closenav()
+{
+	mobileNavSideBar.style.display = "none";
+}
+
+window.addEventListener("resize", function()
+{
+    if (document.documentElement.clientWidth > 1050) closenav();
+}, true);
